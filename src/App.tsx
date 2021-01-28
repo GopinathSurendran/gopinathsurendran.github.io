8e280bfd88f0586ee8c6553f459d05e03f04916e
GopinathSurendran <sgopinath.sms@gmail.com>
2021-01-27 20:12:34 -0500

    test cicd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          Testing github CI CD
         </a>
       </header>
     </div>
