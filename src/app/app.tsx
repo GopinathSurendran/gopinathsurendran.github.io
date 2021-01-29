f3872cabb51e46f296c2d05b2df476518381d0eb
GopinathSurendran <sgopinath.sms@gmail.com>
2021-01-29 17:28:43 -0500

    updated app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './app.scss';
-import Layout from './containers/layout/layout';
-import Home from './containers/home/home';
+import Layout from '../containers/layout/layout';
+import Home from '../containers/home/home';
 
 function App() {
   return (
