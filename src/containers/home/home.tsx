fd6b18e63441661e170b970e0a39267791d4f589
Gopinath Surendran <sgopinath.sms@gmail.com>
2021-11-08 16:59:15 -0500

    Added abilities

--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -3,12 +3,14 @@ import classes from './home.module.scss';
 import Intro from '../intro/intro';
 import Profile from '../profile/profile';
 import Experiences from '../experiences/experiences';
+import Abilities from '../abilities/abilities';
 
 const Home = () => {
     return <div>
         <Intro />
         <Profile />
         <Experiences />
+        <Abilities />
     </div>
 }
 
